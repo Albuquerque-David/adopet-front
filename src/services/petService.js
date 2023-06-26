a92e3e25691b1f630526ba153f1baa977a9d6e87
David Albuquerque <davidra_rj@hotmail.com>
2023-06-26 02:36:20 -0300

    chore: changed env variable name

--- src/services/petService.js
+++ src/services/petService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import FormData from "form-data"
 
-const BASE_URL = process.env.API_URL || "http://localhost:3001/api";
+const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:3001/api";
 
 export const http = axios.create({
   baseURL: BASE_URL,
