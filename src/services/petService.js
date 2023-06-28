dd139319b1406d01d3145fc7f903bed94f6af442
David Albuquerque <davidra_rj@hotmail.com>
2023-06-27 23:49:28 -0300

    chore: removed app json

--- src/services/petService.js
+++ src/services/petService.js
@@ -4,10 +4,7 @@ import FormData from "form-data"
 const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:3001/api";
 
 export const http = axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  }
+  baseURL: BASE_URL
 });
 
 export const getAllPet = async () => {
