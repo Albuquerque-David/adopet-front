bcf41fd4fe91515b19bae63e4462508ef7af95da
David Albuquerque <davidra_rj@hotmail.com>
2023-06-27 21:18:42 -0300

    testing cors

--- src/services/petService.js
+++ src/services/petService.js
@@ -5,7 +5,6 @@ const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:3001/api";
 
 export const http = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true,
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers": "Authorization", 
